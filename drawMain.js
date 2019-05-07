fb4b519373dbe9f086fc891539c78d3a5b2eb678
ShaopengLin <39098000+ShaopengLin@users.noreply.github.com>
2019-05-08 01:39:42 +0800

    yasss

--- drawMain.js
+++ drawMain.js
@@ -5,10 +5,9 @@ var SCREENSTARTX3D = (-side/2);
 var SCREENSTARTY3D = (-side/2);
 var baseGridXValue = side/40;
 var baseGridYValue = side/40;
-var intervalInit = 0;
+var intervalInit = 1;
 var intervalFinal =5;
-var translationX = 0*baseGridXValue;
-var translationY = 1*baseGridYValue;
+var translationY = 0*baseGridYValue;
 var slope = 1;
 var mx = 0;
 var my = 0;
@@ -23,60 +22,22 @@ function setup() {
     numberOfCylSlider = createSlider(5, 50, 5);
     numberOfCylSlider.position(600,0);
     b.background(255);
-    pg.strokeWeight(3);
-    pg.stroke(0);
-    pg.noFill();
-    pg.smooth();
-    pg.line(pg.width/2,0,pg.width/2,pg.height);
-    pg.line(0,pg.height/2,pg.width,pg.height/2);
-    pg.beginShape();
-    pg.translate(translationX,-translationY);
-    for (var i = SCREENSTARTX3D; i < -SCREENSTARTX3D; i++){
-        pg.vertex(i-SCREENSTARTX3D , slope*(-i)-SCREENSTARTY3D);
-    }
-    pg.endShape();
-
+    drawIntervalLinear(SCREENSTARTX3D,-SCREENSTARTX3D);
     frameRate(60);
 
 }
 //how to rotate a shape but not clear the previous p5.js
 function draw() {
     background(175);
-    angleMode(DEGREES);
-    if (mouseIsPressed){
-    if (mouseX < 600 && mouseY < 600){
-        mx += -(pmouseX-mouseX)*0.7;
-        my += (pmouseY-mouseY)*0.7;
-        }
-    }
-    rotateY(mx);
-    rotateX(my);
-    push();
-    texture(b);
-    strokeWeight(1);
-    stroke(0);
+    drawIntervalLinear(intervalInit*baseGridXValue,intervalFinal*baseGridXValue);
+    rotateCanvas();
+    drawLinearVolumnEstimate();
     //interval 1-5
     push();
-    //initial fixation
-    translate(baseGridXValue * (intervalFinal - intervalInit) / numberOfCylSlider.value() / 2+intervalInit * baseGridXValue , 0);
-    //vertical and horizontal translation
-    translate(translationX,-translationY);
-    for(var i = 0; i < numberOfCylSlider.value(); i++){
-        push(); 
-        //translate length of each cylinder
-        translate(i*baseGridXValue * (intervalFinal - intervalInit) / numberOfCylSlider.value(),0);
-        //rotate by 90 degree
-        rotateZ(90);
-        //draw cylinder
-        
-        cylinder(calculateCylinderRadiusLinear(slope*(i / numberOfCylSlider.value()) * baseGridXValue*(intervalFinal-intervalInit) + slope*intervalInit*baseGridXValue, slope*(i + 1) / numberOfCylSlider.value() * baseGridXValue*(intervalFinal-intervalInit) + slope*intervalInit*baseGridXValue), baseGridXValue * (intervalFinal-intervalInit) / numberOfCylSlider.value());
-        pop();      
-    }
-    
-    pop();
     texture(pg);
     translate(-width/2,-height/2,0);
     rect(0, 0, side, side);
+
     pop();
     /*background(175);
    rotateY(w);
@@ -114,14 +75,66 @@ function draw() {
     
 }*/
 //need input later interval 0-5
-function getLinearLeftX(){
-    return slope*((i / numberOfCylSlider.value()) * baseGridXValue*(intervalFinal-intervalInit) +intervalInit*baseGridXValue+translationY);
+function getLinearLeftX(i){
+    return slope*((i / numberOfCylSlider.value()) * baseGridXValue*(intervalFinal-intervalInit) + intervalInit*baseGridXValue) + translationY;
 }
-function getLinearRightX(){
-    return slope*((i + 1) / numberOfCylSlider.value() * baseGridXValue*(intervalFinal-intervalInit) + intervalInit*baseGridXValue+translationY);
+function getLinearRightX(i){
+    return slope*(i + 1) / numberOfCylSlider.value() * baseGridXValue*(intervalFinal-intervalInit) + slope*intervalInit*baseGridXValue+ translationY;
 }
 function calculateCylinderRadiusLinear(leftXcoord, rightXcoord){
     return (leftXcoord+rightXcoord)/2;
+}
+function drawLinearVolumnEstimate(){
+    push();
+    texture(b);
+    strokeWeight(1);
+    stroke(0);
+    //initial fixation
+    translate(baseGridXValue * (intervalFinal - intervalInit) / numberOfCylSlider.value() / 2+intervalInit * baseGridXValue , 0);
+    //vertical translation
+    for(var i = 0; i < numberOfCylSlider.value(); i++){
+        push(); 
+        //translate length of each cylinder
+        translate(i*baseGridXValue * (intervalFinal - intervalInit) / numberOfCylSlider.value(),0);
+        //rotate by 90 degree
+        rotateZ(90);
+        //draw cylinder
+        
+        cylinder(calculateCylinderRadiusLinear(getLinearLeftX(i),getLinearRightX(i)) , baseGridXValue * (intervalFinal-intervalInit) / numberOfCylSlider.value());
+        pop();      
+    }
+    pop();
+}
+function drawIntervalLinear(start , end){
+    pg.clear();
+    pg.strokeWeight(3);
+    pg.stroke(0);
+    pg.noFill();
+    pg.smooth();
+    pg.line(pg.width/2,0,pg.width/2,pg.height);
+    pg.line(0,pg.height/2,pg.width,pg.height/2);
+    pg.strokeWeight(6);
+    pg.stroke(255,0,0);
+
+    pg.beginShape();
+    pg.translate(0,-translationY);
+    for (var i = start; i < end; i++){
+        pg.vertex(i-SCREENSTARTX3D , slope*(-i)-SCREENSTARTY3D);
+    }
+    pg.endShape();
+}
+function rotateCanvas(){
+    push();
+    angleMode(DEGREES);
+    if (mouseIsPressed){
+    if (mouseX < mainCanvas.x+side && mouseY < mainCanvas.y+side){
+        mx += -(pmouseX-mouseX)*0.7;
+        my += (pmouseY-mouseY)*0.7;
+        }
+    }
+    rotateY(mx);
+    rotateX(my);
+    pop();
 }
 function outputname(){
     var input;
