2653a44457b2e1513fe2766e2a450da32efbe973
MichaelZhang333 <47325244+MichaelZhang333@users.noreply.github.com>
2019-05-10 11:22:33 +0800

    User Interface

---
+++ function revolution.js
@@ -0,0 +1,68 @@
+
+
+function setup(){
+
+}
+
+function draw(){
+	console.log(linearVerticalShift());
+}
+
+function lowerBound() {
+	var lowerBound = document.getElementById("lowerBound").value;
+	return parseFloat(lowerBound, 10);
+}
+
+function upperBound() {
+	var upperBound = document.getElementById("upperBound").value;
+	return parseFloat(upperBound, 10);
+}
+
+function subintervals(){
+	var subintervals = document.getElementById("subintervals").value;
+	return parseFloat(subintervals, 10);
+}
+
+function linearSlope(){
+	var linearFunction = document.getElementById("function1").value;
+	if(linearFunction.search("x") == -1){
+		return 0;
+	}
+	else if(linearFunction.search("x") == 0){
+		return 1;
+	}
+	else{
+		var slope = linearFunction.substr(0, linearFunction.search("x"));
+		return parseFloat(slope, 10);
+	}
+}
+
+function linearVerticalShift(){
+	var linearFunction = document.getElementById("function1").value;
+	if(linearFunction.search("x") == -1){
+		return linearFunction;
+	}
+	else{
+		var verticalShift = linearFunction.substr(linearFunction.search("x") + 1, linearFunction.length);
+		return parseFloat(verticalShift, 10);
+	}
+}
+
+
+
+//function linear
+
+//var x;
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+
+
+// Update the current slider value (each time you drag the slider handle)
+//slider.oninput = function() {
+  //output.innerHTML = this.value;
+//}
+
+
+
+
+	
